70301737c5b96490e1b23a4658a78278d98a413c
Wyn Mars <wynma00@gmail.com>
2023-02-08 15:21:49 -0800

    discard config file, use env.

--- routes/api/openai.js
+++ routes/api/openai.js
@@ -1,28 +1,25 @@
-require('dotenv').config()
-const express = require('express')
-const router = express.Router()
-const OpenAIimage = require('../../models/OpenAI')
+require("dotenv").config();
+const express = require("express");
+const router = express.Router();
+const OpenAIimage = require("../../models/OpenAI");
 
-
-const { Configuration, OpenAIApi } = require('openai')
+const { Configuration, OpenAIApi } = require("openai");
 
 const configuration = new Configuration({
   apiKey: process.env.REACT_APP_OPENAI_API_KEY,
 });
-const openai = new OpenAIApi(configuration)
+const openai = new OpenAIApi(configuration);
 
-router.post('/', async (req, res) => {
-  console.log('OpenAI Page Post')
+router.post("/", async (req, res) => {
+  console.log("OpenAI Page Post");
 
   try {
-   
     const response = await openai.createImage({
       prompt: req.body.prompt,
       n: 1,
-      size: '256x256',
-    })
+      size: "256x256",
+    });
 
-    image_url = response.data.data[0].url
 
     ////Save to MongoDB
     // const newImg = await new OpenAIimage({
@@ -32,20 +29,21 @@ router.post('/', async (req, res) => {
 
     // const imageGenerator = await newImg.save()
 
-    res.send(image_url)
-    console.log(image_url)
-  } catch (err) {
-    console.error(err.message)
-    res.status(500).send('Server Error')
+  if (response.data) {
+            if (response.data.data[0].url) {
+                return res.status(200).json(response.data.data[0].url);
+            }
+        }
+    } catch (err) {
+        return res.status(404).json({ message: err.message});
   }
-})
+});
 
 // router.get('/', (req, res) => res.send('OpenAI Page'))
 
-
 router.get("/", async (req, res) => {
   try {
-    console.log('OpenAI Get Request')
+    console.log("OpenAI Get Request");
     const posts = await OpenAIimage.find();
     res.json(posts);
   } catch (err) {
@@ -54,4 +52,4 @@ router.get("/", async (req, res) => {
   }
 });
 
-module.exports = router
+module.exports = router;
